a9b924ad59a23791bf5f9c2abc7a3fb5c518ca0c
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-22 19:51:59 +0100

    add iframe

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,18 @@ export default function App() {
         height: "100vh",
       }}
     >
-      <div style={{ width: 500, height: 500 }}>
+      {/* Tldraw Component */}
+      <div style={{ width: 500, height: 500, marginRight: '10px' }}>
         <Tldraw></Tldraw>
+      </div>
+
+      {/* Iframe Component */}
+      <div style={{ flex: 1 }}>
+        <iframe
+          src="https://jupyterlite.github.io/xeus-python-demo/repl"
+          style={{ width: '100%', height: '100%' }}
+          title="Your Iframe Title"
+        ></iframe>
       </div>
     </div>
   );
